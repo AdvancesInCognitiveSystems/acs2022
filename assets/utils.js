4c0e09343e4202770801358aa8be41d7113c4e13
markburstein-sift <burstein@sift.net>
2022-11-10 18:04:02 -0500

    work on auto populate papers

--- assets/utils.js
+++ assets/utils.js
@@ -1,21 +1,44 @@
 
 const paperMap = {}
+var data
 
-function processPapers(data) {
-  console.log('papers:', data)
-  const rowObjs = $.csv.toObjects(data)
-  console.log('row objs:', rowObjs)
-
-//  for (const rowObj of rowObjs) {
-//    const rowNum = rowObj['#']
-//    if (parseInt(rowNum)) {
-//      paperMap[rowNum] = rowObj
-//    }
-//  }
-
-//  console.log('paperMap', paperMap)
-
+function getRows(data) {
+    const rowObjs = $.csv.toObjects(data)
+    console.log('paper rows:', rowObjs)
+    return rowObjs
 }
+
+function divID (d) {
+    d.getAttribute('id')
+}
+
+function populateCat(catDiv) {
+    console.log("top of populateCat", catDiv)
+    catID = catDiv.getAttribute('id')
+    catDiv.innerHtml = "<ul><li>" + "cat" + catID + "<li><ul>"
+    console.log('populated:', catID, catDiv.innerHtml)
+}
+
+// called in onload <script> of papers-by-topic.md
+function populateCategories () {
+    catDivs = document.getElementsByClassName("PaperCat")
+    console.log('populateCategories divs:', catDivs, catDivs.length)
+    for(let i = 0; i < catDivs.length; i++) { 
+        console.log("div", i, catDivs[i])
+        populateCat(catDivs[i])
+    }
+}
+
+
+// was to be called onload of body from default.html
+function showPapers() {
+    if (document.getElementById("PapersByTopic")) {
+        console.log("top of show papers found PapersByTopic")
+        populateCategories()
+    }
+}
+
+
 
 function slackChannel(paperNum) {
   const channelUrl = paperMap[paperNum]["Slack Channel"]
@@ -30,3 +53,14 @@ function goToSlackChannel(paperNum) {
   console.log('openSlackChannel', url)
   window.open(url,'_blank')
 }
+
+
+function loadCSV(name) {
+    $.ajax({
+        type: "GET",
+        url: "{{ site.baseurl }}/" + name,
+        dataType: "text",
+        success: function(response) {data = getRows(response);}
+    });
+}
+    
